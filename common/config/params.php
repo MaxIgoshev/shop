0b37b1e1065514708396513400a5f0d487286972
max <igoshevmax@gmail.com>
2018-09-25 09:50:39 +0300

    begin fix

---
+++ common/config/params.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'cookieDomain' => '.example.com',
+    'frontendHostInfo' => 'http://shop.dev',
+    'backendHostInfo' => 'http://backend.shop.dev',
+];
